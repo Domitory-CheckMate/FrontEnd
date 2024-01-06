3ef3f27842ab1a0f7b626f4c75fec175e4ff3b94
Ginieee <kaj1226@naver.com>
2024-01-06 17:35:21 +1100

    [Fix] 온보딩/MBTI 레이아웃 수정

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -14,10 +14,10 @@ const OnboardingPage = () => {
           isAble={true}
           onClick={() => navigate('/onboarding/2')}
         />
-        <div className="w-full flex flex-col items-center mb-[53px] text-[10px] leading-[14px] text-[#9D9D9D] font-semibold">
+        <div className="w-full flex flex-col items-center mb-[53px] text-[10px] leading-[14px] text-grayScale4 font-semibold">
           <div>회원가입 후 서비스를 이용할 수 있습니다.</div>
           <div>
-            <span className="text-[#6F6F6F]">이용약관 및 학교인증</span>을
+            <span className="text-grayScale5">이용약관 및 학교인증</span>을
             진행해주세요.
           </div>
         </div>
